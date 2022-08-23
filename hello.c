020c17f39170dafd5bd0b5ed8b7c95032017ba27
Jack Williames <Philbert5600@gmail.com>
2022-08-23 17:11:44 +0000

    added hello world, name, and major to output.

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Jack Williames
+ *         EMAIL
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("It's me, Jack Williames!\n");
+  printf("My major is Computer Science\n");
+
+  return 0;
+}
